945eb34d241c4dbf81a3ed11fbc7921430c22224
SIMLUKE <luc.simon@epitech.eu>
2024-10-21 12:25:05 +0200

    fix: added dwall in the rofi ecosystem

--- my_dwall/my_dwall/change_conf.sh
+++ my_dwall/my_dwall/change_conf.sh
@@ -2,20 +2,21 @@
 
 source ~/.config/my_dwall/dwall_func.sh
 
-selected=$(find "$HOME/.config/my_dwall/images/" -mindepth 1 -maxdepth 1 -type d -print0 |
-    while IFS= read -r -d '' dir; do
-        if [ -f "$dir/12.png" ]; then
-            echo -en "img:$dir/12.png\x00icon\x1f$dir\n"
-        elif [ -f "$dir/12.jpg" ]; then
-            echo -en "img:$dir/12.jpg\x00icon\x1f$dir\n"
-        fi
-    done |
-    wofi -d -i -s ~/.config/wofi/wpp_wofi/style.css -c ~/.config/wofi/wpp_wofi/config)
-
+selected=$(
+    find "$HOME/.config/my_dwall/images/" -mindepth 1 -maxdepth 1 -type d -print0 |
+        while IFS= read -r -d '' dir; do
+            if [ -f "$dir/12.png" ]; then
+                echo -en "$(basename $dir)/\0icon\x1f$dir/12.png\n"
+            elif [ -f "$dir/12.jpg" ]; then
+                echo -en "$(basename $dir)/\0icon\x1f$dir/12.jpg\n"
+            fi
+        done | rofi -dmenu -theme ~/.config/rofi/style-10WALL.rasi
+)
 echo $selected
 
 if [ -n "$selected" ]; then
-    dir_name=$(basename "$(dirname "$selected")")
+    #dir_name=$(basename "$(dirname "$selected")")
+    dir_name=$selected
     echo "$dir_name" >~/.config/my_dwall/wallpaper.conf
     update_wallpaper
 fi
